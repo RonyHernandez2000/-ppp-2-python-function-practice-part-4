021f8d555c976514ec4e5e4f8a69f01a393b4978
Rony Hernandez <ronyhernandez3314@gmail.com>
2022-04-18 17:50:50 -0400

    finished

--- main.py
+++ main.py
@@ -1,48 +1,41 @@
-# first prompt
+# 1
+def max_num(num1, num2, num3):
+    return max(num1, num2, num3)
 
-def max_num(a,b,c):
-  return max([a,b,c])
+print(max_num(80, 7, 4))
+print(max_num(65, 13, 6))
+print(max_num(100, 23, 8))
 
-print(max_num(1,2,3))
-print(max_num(100,50,1))
-print(max_num(15,30,2))
+# 2
+def mult_list(list):
+    result = 1
+    for num in list:
+        result = result * num
+    return result
 
-# Second Prompt
+print(mult_list([4, 6, 3, 9]))
 
-def mult_list(lst):
-  if len(lst) == 0:
-    return 0 
-  prod = lst[0]
-  if len(lst) > 1:
-    for i in lst[1:]:
-      prod = prod * i
+#3
+def rev_string(String):
+    return String[::-1]
 
-  return prod
-    
-print(mult_list([1,2,3]))
-print(mult_list([]))
-print(mult_list([15]))
+print(rev_string("Hello world"))
 
-# Third Prompt
-def rev_string(my_str):
-  return my_str[::-1]
 
-print(rev_string(""))
-print(rev_string("apple"))
-print(rev_string("mt string"))
+# 4
+def num_within(num, start_num, start_end):
+    if num in range(start_num, start_end + 1):
+        return True
+    else:
+        return False 
+print(num_within(4, 3, 2))
+print(num_within(5, 2, 5))
+print(num_within(11, 3, 6))
 
-# Fourth Prompt
-
-def num_within(x,a,b):
-  return x in range(a,b+1)
-     
-print(num_within(3,2,4))     
-print(num_within(3,1,3))     
-print(num_within(10,2,5))
-
-# Fifth Prompt
+#5
 triangle = [[1],[1,1]]
 def pascal(n):
+  
   if n < 1:
     print("invalid number of rows")
   elif n == 1:
@@ -65,5 +58,5 @@ def pascal(n):
     for row in triangle:
       print(row)
 
-pascal(2)
+
 pascal(5)
